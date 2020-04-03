af6f004ba6aac5c089d46fd2b3e6a0206b4ffe5d
No One <franklinquintin82@gmail.com>
2020-04-02 19:58:24 -0500

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Boy Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Lil Boy Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Not a Boy Cheeseburger', true);
